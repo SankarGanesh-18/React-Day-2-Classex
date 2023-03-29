899ced427531c1e0b6b3a09575b9d90f85499a80
SankarGanesh-18 <122220555+SankarGanesh-18@users.noreply.github.com>
2023-03-29 11:50:31 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div style={{backgroundColor:'whitesmoke',bottom:'0',position:'Fixed',width:'100%'
+}}>
+    Follow us on:
+    <a href='https://www.instagram.com/ferrari/?hl=en'>Instagram</a>
+</div>
+  )
+}
+
